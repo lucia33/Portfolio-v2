61f645392f10a6ab76198bc8f01bdeb08ed32746
lucia33 <liu_chang2010@hotmail.com>
2015-11-15 23:55:24 -0500

    changing mongoldb due to failure of previous one

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ module.exports = {
 
 // 'url':'mongodb://localhost/userDB' //localdb
 
-'url': 'mongodb://lucia:liuchang@ds048368.mongolab.com:48368/changliu' //mongolab
-// 'url': 'mongodb://lucia:liuchang@ds042138.mongolab.com:42138/lucia' //mongolab
+// 'url': 'mongodb://lucia:liuchang@ds048368.mongolab.com:48368/changliu' //mongolab
+'url': 'mongodb://lucia:liuchang@ds042138.mongolab.com:42138/lucia' //mongolab2
 
 
 };
